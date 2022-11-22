a7ef5e342194db2d94927e33ef6674d505baa672
sezasada <111367099+sezasada@users.noreply.github.com>
2022-11-22 10:11:09 -0800

    Update array-practice.js
    Fixed.

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -8,7 +8,7 @@ let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
-// Testing... 
+ let favoriteFoods = ['pasta', 'burger', 'pizza'];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
